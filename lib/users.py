6ac65bb7582ac35f1aaccb85cabf9883b5a2ee54
GeorgiVL <77583373+GeorgiVL@users.noreply.github.com>
2022-11-07 12:42:43 +0200

    Update users.py

--- lib/users.py
+++ lib/users.py
@@ -4,14 +4,12 @@ from lib.utils import build_request_headers
 
 
 class Users:
-
-    def __init__(self):
-        self.user_url = "/users"
-
+    USER_URL = "/users"
+    
     def get_current_users(self, app_url, access_token):
         LOG.info("get_current_users")
         request_headers = build_request_headers(access_token)
-        response = SESSION.get(f"{app_url}{self.user_url}/", headers=request_headers)
+        response = SESSION.get(f"{app_url}{self.USER_URL}/", headers=request_headers)
         LOG.debug(response.json())
 
         return response
@@ -19,7 +17,7 @@ class Users:
     def get_user_id(self, app_url, access_token):
         LOG.info("get_user_id")
         request_headers = build_request_headers(access_token)
-        response = SESSION.get(f"{app_url}{self.user_url}/", headers=request_headers)
+        response = SESSION.get(f"{app_url}{self.USER_URL}/", headers=request_headers)
         LOG.debug(response.json()[1]["id"])
 
         return response.json()[1]["id"]
@@ -34,7 +32,7 @@ class Users:
         }
 
         LOG.debug(f"Request payload: {payload}")
-        response = SESSION.post(f"{app_url}{self.user_url}/", headers=request_headers, json=payload)
+        response = SESSION.post(f"{app_url}{self.USER_URL}/", headers=request_headers, json=payload)
         LOG.info(response.json())
 
         return response
@@ -42,7 +40,7 @@ class Users:
     def get_current_authenticated_user(self, app_url, access_token):
         LOG.info("get_current_authenticated_user")
         request_headers = build_request_headers(access_token)
-        response = SESSION.get(f"{app_url}{self.user_url}/me", headers=request_headers)
+        response = SESSION.get(f"{app_url}{self.USER_URL}/me", headers=request_headers)
         LOG.debug(response.json())
 
         return response
@@ -50,7 +48,7 @@ class Users:
     def delete_an_user(self, app_url, access_token, user_id):
         LOG.info("delete_an_user")
         request_headers = build_request_headers(access_token)
-        response = SESSION.delete(f"{app_url}{self.user_url}/{user_id}", headers=request_headers)
+        response = SESSION.delete(f"{app_url}{self.USER_URL}/{user_id}", headers=request_headers)
         LOG.debug(response.json())
 
         return response
